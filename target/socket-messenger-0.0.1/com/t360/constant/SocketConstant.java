cc88c837a2c9755dd0ed66ae09a2937a62e33c9a
ibesli <ibesli@MacbookPros-MacBook-Pro.local>
2017-03-04 21:02:35 +0300

    socket example

---
+++ target/socket-messenger-0.0.1/com/t360/constant/SocketConstant.java
@@ -0,0 +1,14 @@
+package com.t360.constant;
+
+public class SocketConstant {
+
+	public static final String	SERVER						= "server";
+	public static final String	CLIENT						= "client";
+	public static final String	SOCKET_PORT_NUMBER			= "socketPortNumber";
+	public static final int		MAX_TRIAL_NUMBER			= 3;
+	public static final int		DEFAULT_PORT				= 9999;
+	public static final int		DEFAULT_NUMBER_OF_CLIENT	= 2;
+
+	private SocketConstant() {
+	}
+}
